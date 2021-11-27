86a48b5842b25f61610dd247238317c7788042b1
Jahangir Alam <geostruc.samraat@gmail.com>
2021-11-27 15:22:38 +0600

    dynamic data fetch and slug by data show

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <template>
 <div>
   <h1 class="text-center">Customer Bio Table</h1>
-    <ul v-for="mountain in mountains" :key="mountain.id">
+    <ul v-for="mountain in mountains" :key="mountain.slug">
       <li>{{mountain.name}}</li>
+       <NuxtLink :to="`mountains/${mountain.slug}`">{{
+          mountain.email
+        }}</NuxtLink>
     </ul>
 </div>
 </template>
---
+++ pages/mountains/_slug.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ mountain.name }}</h1>
+    <h2>{{ mountain.email }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios, params }) {
+    const mountain = await $axios.$get(
+      `http://localhost:8000/api/customer/${params.slug}`
+    )
+    return { mountain }
+  }
+}
+</script>
+<style scoped>
+p {
+  margin: 20px auto;
+  max-width: 600px;
+}
+</style>
