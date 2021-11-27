688650fd2595022dee3e7b8ad8700d0db0cb01d4
Jahangir Alam <geostruc.samraat@gmail.com>
2021-11-27 15:56:16 +0600

    axios wise data read

---
+++ pages/customers/_slug.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ customer.name }}</h1>
+    <h2>{{ customer.email }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios, params }) {
+    const customer = await $axios.$get(
+      `http://localhost:8000/api/customer/${params.slug}`
+    )
+    return { customer }
+  }
+}
+</script>
+<style scoped>
+p {
+  margin: 20px auto;
+  max-width: 600px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,72 @@
 <template>
-<div>
-  <h1 class="text-center">Customer Bio Table</h1>
-    <ul v-for="mountain in mountains" :key="mountain.slug">
+  <div class="container">
+    <h1 class="text-center">Customer Bio Table</h1>
+    <div class="card">
+      <div class="card-header">
+        User Information
+      </div>
+      <div class="card-body">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(customer, index) in customers" :key="customer.slug">
+              <td># {{ index + 1 }}</td>
+              <td> {{ customer.name }}</td>
+              <td>{{ customer.email }}</td>
+              <td>
+                <nuxt-link :to="`customers/${customer.slug}`">Edit</nuxt-link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <!-- <ul v-for="mountain in mountains" :key="mountain.slug">
       <li>{{mountain.name}}</li>
        <NuxtLink :to="`mountains/${mountain.slug}`">{{
           mountain.email
         }}</NuxtLink>
-    </ul>
-</div>
+    </ul> -->
+  </div>
 </template>
 
 <script>
+// const axios = require('axios');
+import axios from 'axios'
 export default {
-  async asyncData({ $axios }) {
-    const mountains = await $axios.$get(`http://localhost:8000/api/customer`)
-    return { mountains }
-  }
+  data() {
+    return {
+      customers: {},
+    }
+  },
+
+  mounted() {
+    console.log('Component mounted.')
+    this.getData()
+  },
+  methods: {
+    getData() {
+      axios
+        .get('http://localhost:8000/api/customer')
+        .then((response) => {
+          this.customers = response.data
+          console.log(response.data)
+        })
+        .catch((e) => {
+          console.log(e)
+        })
+    },
+  },
+  // async asyncData({ $axios }) {
+  //   const mountains = await $axios.$get(`http://localhost:8000/api/customer`)
+  //   return { mountains }
+  // }
 }
 </script>
