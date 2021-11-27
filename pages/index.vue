f9de68b8118fd485261057a536707fff2a6d996a
Jahangir Alam <geostruc.samraat@gmail.com>
2021-11-27 14:48:32 +0600

    axios data get

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,17 @@
 <template>
-  <Tutorial/>
+<div>
+  <h1 class="text-center">Customer Bio Table</h1>
+    <ul v-for="mountain in mountains" :key="mountain.id">
+      <li>{{mountain.name}}</li>
+    </ul>
+</div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $axios }) {
+    const mountains = await $axios.$get(`http://localhost:8000/api/customer`)
+    return { mountains }
+  }
+}
 </script>
